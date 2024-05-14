c24aa51000aa6883c32517d6efb0df84fdd1335f
shangcom <dndbs91@gmail.com>
2024-05-14 18:09:43 +0900

    Initial commit

---
+++ src/main/java/org/zerock/bmine01/BMine01Application.java
@@ -0,0 +1,13 @@
+package org.zerock.bmine01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BMine01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BMine01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/bmine01/BMine01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.bmine01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BMine01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
