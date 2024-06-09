d7fedd2c3352132cac3b13e41b61a23f1f0064e5
shangcom <dndbs91@gmail.com>
2024-06-09 22:31:31 +0900

    p471 @Builder 어노테이션 정리했음.

--- src/main/java/org/zerock/bmine01/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/bmine01/dto/PageRequestDTO.java
@@ -4,6 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 
 @Builder
 @Data
@@ -17,7 +24,42 @@ public class PageRequestDTO {
     @Builder.Default
     private int size = 10;
 
-    private String type;
+    private String type; // 검색의 종류 t, c, w, tc, tw, cw, twc
 
     private String keyword;
+
+
+    private String link; // 페이징과 검색 조건을 문자열로 구성
+
+    public String[] getTypes() {
+        if (type == null || type.isEmpty()) {
+            return null;
+        }
+        return type.split("");
+    }
+
+    public Pageable getPageable(String... props) {
+        return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
+    }
+
+    public String getLink() {
+        if (link == null) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("page=" + this.page);
+            stringBuilder.append("&size=" + this.size);
+            if (type != null && type.length() > 0) {
+                stringBuilder.append("&type=" + this.type);
+            }
+            if (keyword != null) {
+                try {
+                    stringBuilder.append("&keyword=" + URLEncoder.encode(keyword, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            link = stringBuilder.toString();
+        }
+        return link;
+
+    }
 }
