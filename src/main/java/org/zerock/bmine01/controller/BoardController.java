1433e0ee59215ceef13824755a3c50aac5f61b21
shangcom <dndbs91@gmail.com>
2024-06-15 21:21:38 +0900

    p412 스프링 부트에서 웹 개발 정리중 타임리프와 api server 비교

---
+++ src/main/java/org/zerock/bmine01/controller/BoardController.java
@@ -0,0 +1,25 @@
+package org.zerock.bmine01.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.bmine01.dto.PageRequestDTO;
+import org.zerock.bmine01.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+    }
+
+}
