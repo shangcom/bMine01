ef1c0495b48a52eed0b0206b03dcd362f96b43c8
shangcom <dndbs91@gmail.com>
2024-06-22 21:58:25 +0900

    p483 검색 화면 추가부터 하면 됨.

--- src/main/java/org/zerock/bmine01/controller/BoardController.java
+++ src/main/java/org/zerock/bmine01/controller/BoardController.java
@@ -16,7 +16,7 @@ import org.zerock.bmine01.service.BoardService;
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
-
+////
     private final BoardService boardService;
 
     //list 메서드는 아무 것도 반환하지 않지만, 뷰 이름을 명시하지 않을 경우,
@@ -24,12 +24,14 @@ public class BoardController {
     // src/main/resources/templates/board/list.html 템플릿을 찾음.
     // localhost:8080/board/list를 요청할 때, list.html 템플릿에서 responseDTO 데이터를 사용할 수 있게 됨.
     @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model model) {
-
+    public String list(PageRequestDTO pageRequestDTO, Model model) {
+        // 파라미터 바인딩 : HTTP 요청 파라미터는 스프링이 자동으로 컨트롤러 메서드의 매개변수 객체에 매핑함.
+        // 여기서는 PageRequestDTO, Model 객체가 자동으로 생성된다.
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
         log.info(responseDTO);
         model.addAttribute("responseDTO", responseDTO);
+        return "board/list";
         // responseDTO를 view(list.html 템플릿)에 전달됨.
     }
 
--- src/main/java/org/zerock/bmine01/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/bmine01/dto/PageRequestDTO.java
@@ -1,66 +1,66 @@
-package org.zerock.bmine01.dto;
+    package org.zerock.bmine01.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+    import org.springframework.data.domain.PageRequest;
+    import org.springframework.data.domain.Pageable;
+    import org.springframework.data.domain.Sort;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PageRequestDTO {
-
-    @Builder.Default
-    private int page = 1; // 현재 페이지(클라이언트가 보고 있는 페이지 / 보고자 하는 페이지)
-
-    @Builder.Default
-    private int size = 10; // 페이지 크기 (한 페이지에 있는 요소의 수)
-
-    private String type; // 검색의 종류 t, c, w, tc, tw, cw, twc. Board 엔티티의 필드들.
-    // bno, regdate, moddate 등도 가능하나, BoardSearchImpl의 searchAll을 수정해야함.
-
-    private String keyword; // 검색어. 설정한 검색의 종류 중 해당 키워드를 담고 있는 것들을 검색한다.
+    import java.io.UnsupportedEncodingException;
+    import java.net.URLEncoder;
 
 
-    private String link; // 페이징과 검색 조건을 문자열로 구성. URL로 전달하기 위해 필요.
+    @Builder
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class PageRequestDTO {
 
-    public String[] getTypes() {
-        if (type == null || type.isEmpty()) {
-            return null;
-        }
-        return type.split("");
-    }
+        @Builder.Default
+        private int page = 1; // 현재 페이지(클라이언트가 보고 있는 페이지 / 보고자 하는 페이지)
 
-    public Pageable getPageable(String... props) {
-        return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
-    } // this.page -1 :
+        @Builder.Default
+        private int size = 10; // 페이지 크기 (한 페이지에 있는 요소의 수)
 
-    public String getLink() {
-        if (link == null) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("page=" + this.page);
-            stringBuilder.append("&size=" + this.size);
-            if (type != null && type.length() > 0) {
-                stringBuilder.append("&type=" + this.type);
+        private String type; // 검색의 종류 t, c, w, tc, tw, cw, twc. Board 엔티티의 필드들.
+        // bno, regdate, moddate 등도 가능하나, BoardSearchImpl의 searchAll을 수정해야함.
+
+        private String keyword; // 검색어. 설정한 검색의 종류 중 해당 키워드를 담고 있는 것들을 검색한다.
+
+
+        private String link; // 페이징과 검색 조건을 문자열로 구성. URL로 전달하기 위해 필요.
+
+        public String[] getTypes() {
+            if (type == null || type.isEmpty()) {
+                return null;
             }
-            if (keyword != null) {
-                try {
-                    stringBuilder.append("&keyword=" + URLEncoder.encode(keyword, "UTF-8"));
-                } catch (UnsupportedEncodingException e) {
-                    throw new RuntimeException(e);
+            return type.split("");
+        }
+
+        public Pageable getPageable(String... props) {
+            return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
+        } // this.page -1 :
+
+        public String getLink() {
+            if (link == null) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("page=" + this.page);
+                stringBuilder.append("&size=" + this.size);
+                if (type != null && type.length() > 0) {
+                    stringBuilder.append("&type=" + this.type);
                 }
+                if (keyword != null) {
+                    try {
+                        stringBuilder.append("&keyword=" + URLEncoder.encode(keyword, "UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                link = stringBuilder.toString();
             }
-            link = stringBuilder.toString();
-        }
-        return link;
+            return link;
 
+        }
     }
-}
